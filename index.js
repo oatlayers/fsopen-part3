41995ca62a080aa9bf6d3f3902d435bbccc18b62
oatlayers <71181864+oatlayers@users.noreply.github.com>
2024-01-15 17:50:09 +0700

    finished exercise part 3.1 until 3.6

---
+++ index.js
@@ -0,0 +1,97 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+// display all persons
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+// display amount and date of request
+app.get('/info', (req, res) => {
+    const timestamp = new Date()
+    const length = persons.length
+    const htmlString = `<p>Phonebook has info for ${length} people</p><p>${timestamp}</p>`
+  
+    res.send(htmlString)
+})
+
+// go to specific resource
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(p => p.id === id)
+
+    if (person) {
+        res.json(person)
+    } else {
+        res.statusMessage = "Can't find it"
+        res.status(404).end()
+    }
+})
+
+// delete resource
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(p => p.id !== id)
+
+    res.status(204).end()
+})
+
+// make new resource and appropriate errors
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+    const name = req.body.name
+
+    console.log('value of name:', name)
+
+    if (!body.number || !body.name) {
+        return res.status(400).json({
+            error: 'number or name is missing'
+        })
+    } else if (persons.find(p => p.name === name)) {
+        return res.status(400).json({
+            error: 'name already exists'
+        })
+    }
+
+    const person = {
+        id: Math.floor(Math.random() * 1000),
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person)
+    
+    res.json(person)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
+
+

02056b0f8520f066472ce2fe23c2013c5a33ce1f
oatlayers <71181864+oatlayers@users.noreply.github.com>
2024-01-29 20:04:33 +0700

    finished 3.12

---
+++ mongo.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose')
+
+// when no password is given, ask for one
+if (process.argv.length < 3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+// variables
+const password = process.argv[2]
+const theName = process.argv[3]
+const theNumber = process.argv[4]
+
+const url = `mongodb+srv://lolcraftlolzz:${password}@oatlayersdb.phmciqz.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+// define schema
+const personSchema = new mongoose.Schema({
+    name: String, 
+    number: Number,
+})
+
+//define model
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+        name: theName,
+        number: theNumber,
+})
+
+// console.log('valuess:', theName, theNumber) 
+// console.log('value of person:', person)
+
+// if there's no name and number display all entries
+// else display message
+if (!theName && !theNumber) {
+    Person.find({}).then(persons => {
+        persons.forEach(person => {
+            console.log(person)
+        })
+        mongoose.connection.close()
+    })
+} else if (theName && theNumber) {
+    person.save().then(result => {
+        console.log(`added ${theName} number ${theNumber} to phonebook`)
+        mongoose.connection.close()
+    })
+}
